e77dcaf97677771ee6da99488787fbe84e31b5b7
wildisign <wildisign@users.noreply.github.com>
2020-12-26 20:56:56 +0900

    1226

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(function(){
-
+	
 	var docStyle = document.documentElement.style;
 
 	// setting CSS variables inside this JS where they 
@@ -12,31 +12,45 @@ $(function(){
 		docStyle.setProperty('--mouse-y', e.clientY - (screen.height / 2));
 	});
 
-  var Box = $(".cont_inner > .section");
-  var timer = 8000;
-  var count = 0;
-    
-  setInterval(function(){
-    count++;
-    var first = count % 5,
-        second = (count + 1) % 5,
-        third = (count + 2) % 5,
-        fourth = (count + 3) % 5,
-        fifth = (count + 4) % 5;
-
-    Box.eq(first).removeClass();
-    Box.eq(first).addClass("section sec0");
-    Box.eq(second).removeClass();
-    Box.eq(second).addClass("section sec1");
-    Box.eq(third).removeClass();
-    Box.eq(third).addClass("section sec2");
-    Box.eq(fourth).removeClass();
-    Box.eq(fourth).addClass("section sec3");
-    Box.eq(fifth).removeClass();
-    Box.eq(fifth).addClass("section sec4");
+	var Box = $(".cont_inner > .section");
+	var timer = 8000;
+	var count = 0;
 	
-	console.log("작동중");
-	  
-  },timer)
+	jQuery(window).blur(function(){
+ 
+    clearInterval(vInterval);
+ 
+	});
+	
+	jQuery(window).focus(function(){
+ 
+    clearInterval(vInterval);
+ 
+	});
+	
+    var vInterval = setInterval( PageFocus, timer );
+	
+	function PageFocus() {
+		
+			count++;
+			var first = count % 5,
+				second = (count + 1) % 5,
+				third = (count + 2) % 5,
+				fourth = (count + 3) % 5,
+				fifth = (count + 4) % 5;
+
+			Box.eq(first).removeClass();
+			Box.eq(first).addClass("section sec0");
+			Box.eq(second).removeClass();
+			Box.eq(second).addClass("section sec1");
+			Box.eq(third).removeClass();
+			Box.eq(third).addClass("section sec2");
+			Box.eq(fourth).removeClass();
+			Box.eq(fourth).addClass("section sec3");
+			Box.eq(fifth).removeClass();
+			Box.eq(fifth).addClass("section sec4");
+			
+			console.log(first);
+	}
 
 });
