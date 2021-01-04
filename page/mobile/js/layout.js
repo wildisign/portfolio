06901c2065b4e9fc105fba54e84bdf0e4c2c5e66
wildisign <wildisign@users.noreply.github.com>
2021-01-04 12:54:12 +0900

    0104

--- page/mobile/js/layout.js
+++ page/mobile/js/layout.js
@@ -1,7 +1,10 @@
 $(function () {
-    $(".tab_content").hide();
+	$('#loading').delay(500).queue(function(){
+	$(this).hide();
+  });
+	
+	$(".tab_content").hide();
     $(".tab_content:first").show();
-
     $("ul.tabs > li").click(function () {
         $("ul.tabs > li").removeClass("active").css("color", "#777");
         //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
