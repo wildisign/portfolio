f8ff60b0d5586d2a06684e1f6031ae243f1aa6f0
wildisign <wildisign@users.noreply.github.com>
2021-01-05 16:40:09 +0900

    0105

--- page/mobile/js/layout.js
+++ page/mobile/js/layout.js
@@ -3,15 +3,32 @@ $(window).on('load', function(){
 		$(this).hide()
 	});
 });
-$(function () {
-	$(".tab_content").hide();
-    $(".tab_content:first").show();
-    $("ul.tabs > li").click(function () {
-        $("ul.tabs > li").removeClass("active").css("color", "#777");
-        //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
-        $(this).addClass("active").css("color", "#fff");
-        $(".tab_content").hide()
-        var activeTab = $(this).attr("rel");
-        $("#" + activeTab).show()
-    });
+	$(function () {
+		$(".tab_content").hide();
+		$(".tab_content:first").show();
+		$("ul.tabs > li").click(function () {
+			$("ul.tabs > li").removeClass("active").css("color", "#777");
+			//$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
+			$(this).addClass("active").css("color", "#fff");
+			$(".tab_content").hide()
+			var activeTab = $(this).attr("rel");
+			$("#" + activeTab).show()
+		});
+	
+	//링크리스트
+	let linkURL = [
+		["https://m.ticket.melon.com/public/index.html#performance.index?prodId=205279"],//1st
+		["https://m.ticket.melon.com/public/index.html#performance.index?prodId=205281"],//2nd
+		["https://m.ticket.melon.com/public/index.html#performance.index?prodId=205333"],//3rd
+	]
+
+	$(".play_url").click(function(event) {
+		// good
+		event.preventDefault();
+        var j = $(".play_url").index(this);  // 존재하는 모든 버튼을 기준으로 index
+        console.log(j);
+		window.open(linkURL[j]); 
+		return false;
+	});	
+	
 });
